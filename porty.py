3a2e83535e5dcd5acf4a71363f556319fcfa542d
Sanskar Kumar Burman <81924416+sanskaros@users.noreply.github.com>
2023-07-23 22:06:05 +0530

    Update porty.py

--- porty.py
+++ porty.py
@@ -1,24 +1,19 @@
-from concurrent.futures.thread import _worker
 import socket
 import threading
-from queue import  Queue
-queue=Queue()
-open_ports=[]
-target=str(input("enter target ip: "))
+from queue import Queue
+
+queue = Queue()
+open_ports = []
+target = input("Enter target IP: ")
+
 def portscan(port):
     try:
-        sock=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-        sock.connect((target,port))
-        return True
-    except:
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
+            sock.settimeout(1)  # Set a timeout for the connection attempt (1 second)
+            result = sock.connect_ex((target, port))
+            return result == 0  # If the result is 0, the port is open; otherwise, it's closed or an error occurred
+    except socket.error:
         return False
-    
-# for port in range(1,1024):
-#     result=portscan(port)
-#     if result:
-#         print("port {} is open!".format(port))
-#     else:
-#         print("port {} is closed".format(port))
 
 def fill_queue(port_list):
     for port in port_list:
@@ -26,19 +21,25 @@ def fill_queue(port_list):
 
 def mainmethod():
     while not queue.empty():
-        port=queue.get()
+        port = queue.get()
         if portscan(port):
-            print("port {} is open!",format(port))
+            print("Port {} is open!".format(port))
             open_ports.append(port)
 
-port_list=range(1,1024)
+port_list = range(1, 1025)  # Include port 1024 in the range
 fill_queue(port_list)
-thread_list=[]
-for t in range(500):
-    thread=threading.Thread(target=mainmethod)
+
+thread_list = []
+num_threads = 100  # Set the number of threads to use (adjust as needed)
+
+for _ in range(num_threads):
+    thread = threading.Thread(target=mainmethod)
     thread_list.append(thread)
+
 for thread in thread_list:
     thread.start()
+
 for thread in thread_list:
     thread.join()
-print("open ports are: ",open_ports)
+
+print("Open ports are:", open_ports)
